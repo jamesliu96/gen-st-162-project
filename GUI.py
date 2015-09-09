1068faeec0d0256ae19dad17e59271c0c03a3ab4
s92025592025 <s92025592025@gmail.com>
2015-09-08 23:05:03 -0700

    finalized Jython GUI

--- GUI.py
+++ GUI.py
@@ -1,120 +1,133 @@
-from javax.swing import *;
-from java.awt import *;
-frame=JFrame("mini_project_04");
-c=frame.getContentPane();
-#JButton startBut, continueBut, confirmBut;
-#JLabel playerNumLabel, RLabel, BLabel, GLabel, toLabel01, toLabel02, toLabel03;
-#JComboBox playerNum;
-#JTextArea gamingProcess;
-#JTextField RValueStart, GValueStart, BValueStart, RValueEnd, GValueEnd, BValueEnd;
-#JPanel colorDisplay;
-#String history;
-frame.setSize(826, 581);
-frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-c.setLayout(null);
-setButtons(); setLabels(); setComboBox(); setTextArea();
-setTextFild(); setPanel();
-frame.setVisible(true);
-		
+import javax.swing as swing;
+import java.awt as awt;
+
 def setButtons():
 	global startBut
-	startBut=JButton("Start!!");
+	startBut=swing.JButton("Start!!");
 	startBut.setSize(69, 27);
 	startBut.setLocation(160, 40);
 	c.add(startBut);
 	global ontinueBut;
-	continueBut=JButton("Continue");
+	continueBut=swing.JButton("Continue");
 	continueBut.setSize(100, 50);
 	continueBut.setLocation(600, 40);
 	c.add(continueBut);
 	global confirmBut;
-	confirmBut=JButton("I belive I'm right!!");
+	confirmBut=swing.JButton("I belive I'm right!!");
 	confirmBut.setSize(250, 100);
 	confirmBut.setLocation(50, 350);
 	c.add(confirmBut);
 def setLabels():
 	global playerNumLabel;
-	playerNumLabel=JLabel("Players: ");
+	playerNumLabel=swing.JLabel("Players: ");
 	playerNumLabel.setSize(75, 20);
 	playerNumLabel.setLocation(80, 30);
-	playerNumLabel.setFont(new Font("Arial", Font.PLAIN, 15));
+	#playerNumLabel.setFont(new Font("Arial", Font.PLAIN, 15));
 	c.add(playerNumLabel);
 	global RLabel;
-	RLabel=JLabel("Red value: ");
+	RLabel=swing.JLabel("Red value: ");
 	RLabel.setSize(73, 18);
 	RLabel.setLocation(50, 250);
 	c.add(RLabel);
 	global GLabel;
-	GLabel=JLabel("Green value: ");
+	GLabel=swing.JLabel("Green value: ");
 	GLabel.setSize(73, 18);
 	GLabel.setLocation(50, 280);
 	c.add(GLabel);
 	global BLabel;
-	BLabel=JLabel("Blue value: ");
+	BLabel=swing.JLabel("Blue value: ");
 	BLabel.setSize(73, 18);
 	BLabel.setLocation(50, 310);
 	c.add(BLabel);
 	global toLabel01;
-	toLabel01=JLabel("to");
+	toLabel01=swing.JLabel("to");
 	toLabel01.setSize(17, 24);
 	toLabel01.setLocation(203, 250);
 	c.add(toLabel01);
 	global toLabel02;
-	toLabel02=JLabel("to");
+	toLabel02=swing.JLabel("to");
 	toLabel02.setSize(17, 24);
 	toLabel02.setLocation(203, 280);
 	c.add(toLabel02);
 	global toLabel03;
-	toLabel03=JLabel("to");
+	toLabel03=swing.JLabel("to");
 	toLabel03.setSize(17, 24);
 	toLabel03.setLocation(203, 310);
 	c.add(toLabel03);
 
 def setComboBox():
-	String[] num={"1", "2"};
-	playerNum=new JComboBox(num);
+	global num;
+	num=["1", "2"];
+	global playerNum;
+	playerNum=swing.JComboBox(num);
 	playerNum.setSize(69, 27);
 	playerNum.setLocation(80, 50);
 	c.add(playerNum);
 
 def setTextArea():
+	global history;
 	history="Welcome to the game!!";
-	gamingProcess=new JTextArea(history);
+	global gamingProcess;
+	gamingProcess=swing.JTextArea(history);
 	gamingProcess.setSize(250, 60);
 	gamingProcess.setLocation(320, 30);
 	c.add(gamingProcess);
 
 def setTextFild():
-	RValueStart=new JTextField();
+	global RValueStart;
+	RValueStart=swing.JTextField();
 	RValueStart.setSize(50, 25);
 	RValueStart.setLocation(150, 250);
 	c.add(RValueStart);
-	GValueStart=new JTextField();
+	global GValueStart;
+	GValueStart=swing.JTextField();
 	GValueStart.setSize(50, 25);
 	GValueStart.setLocation(150, 280);
 	c.add(GValueStart);
-	BValueStart=new JTextField();
+	global BValueStart;
+	BValueStart=swing.JTextField();
 	BValueStart.setSize(50, 25);
 	BValueStart.setLocation(150, 310);
 	c.add(BValueStart);
-	RValueEnd=new JTextField();
+	global RValueEnd;
+	RValueEnd=swing.JTextField();
 	RValueEnd.setSize(50, 25);
 	RValueEnd.setLocation(223, 250);
 	c.add(RValueEnd);
-	GValueEnd=new JTextField();
+	global GValueEnd;
+	GValueEnd=swing.JTextField();
 	GValueEnd.setSize(50, 25);
 	GValueEnd.setLocation(223, 280);
 	c.add(GValueEnd);
-	BValueEnd=new JTextField();
+	global BValueEnd;
+	BValueEnd=swing.JTextField();
 	BValueEnd.setSize(50, 25);
 	BValueEnd.setLocation(223, 310);
 	c.add(BValueEnd);
 
 def setPanel():
-	colorDisplay=new JPanel();
+	global colorDisplay;
+	colorDisplay=swing.JPanel();
 	colorDisplay.setSize(300, 300);
 	colorDisplay.setLocation(400, 150);
-	colorDisplay.setBackground(new Color(255, 255, 255));
+	colorDisplay.setBackground(awt.Color(255, 255, 255));
 	c.add(colorDisplay);
+frame=swing.JFrame("mini_project_04");
+c=frame.getContentPane();
+#swing.JButton startBut, continueBut, confirmBut;
+#swing.JLabel playerNumLabel, RLabel, BLabel, GLabel, toLabel01, toLabel02, toLabel03;
+#swing.JComboBox playerNum;
+#swing.JTextArea gamingProcess;
+#swing.JTextField RValueStart, GValueStart, BValueStart, RValueEnd, GValueEnd, BValueEnd;
+#swing.JPanel colorDisplay;
+#String history;
+frame.setSize(826, 581);
+frame.setDefaultCloseOperation(swing.JFrame.EXIT_ON_CLOSE);
+c.setLayout(None);
+setButtons(); setLabels(); setComboBox(); setTextArea();
+setTextFild(); setPanel();
+frame.setVisible(True);
+		
+
 
 
